8bd3b4bb2a8ef8ce13f4128097672afce7302488
DSergii <drozdsa@gmail.com>
2017-02-26 22:55:26 +0200

    add project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'app works!';
+
+	loginTitle = 'Login';
+
+    regTitle = 'Registration';
+
+    public formRegistration: FormGroup;
+
+    constructor(private fb: FormBuilder) {
+
+    	this.formRegistration = this.fb.group({
+	        email: ['', this.emailValidator],
+	        passwordGroup: this.fb.group({
+	        	password: [''],
+	        	pswconfirm: ['']
+	        })
+	    })
+
+    }
+
+    public emailValidator(control: FormControl): {[key: string]: boolean} {
+
+    	const value = control.value || '';
+    	const valid = value.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/);
+    	return valid ? null : {emailError: true};
+    }
+
+    public loginSubmit(value: any):void {
+    	console.log(value);
+    }
+
+    
+
 }
+
