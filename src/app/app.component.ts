67bfdde1f774662eb8ec21e344dfbe21962bb8f4
DSergii <drozdsa@gmail.com>
2017-03-02 00:00:47 +0200

    asd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/delay';
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
@@ -9,9 +13,9 @@ import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms'
 })
 export class AppComponent {
 
-	loginTitle = 'Login';
+	loginTitle: string = 'Login';
 
-    regTitle = 'Registration';
+    regTitle: string = 'Registration';
 
     public formRegistration: FormGroup;
 
@@ -19,9 +23,13 @@ export class AppComponent {
 
     	this.formRegistration = this.fb.group({
 	        email: ['', this.emailValidator],
+	        agree: ['', this.agreeValidator],
 	        passwordGroup: this.fb.group({
 	        	password: [''],
 	        	pswconfirm: ['']
+	        },
+	        {
+	        	asyncValidator: this.confirmValidator
 	        })
 	    })
 
@@ -32,6 +40,24 @@ export class AppComponent {
     	const value = control.value || '';
     	const valid = value.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/);
     	return valid ? null : {emailError: true};
+
+    }
+
+    public confirmValidator({value}: FormControl): Observable<{[key: string]: boolean}> {
+
+    	const [first, ...rest] = Object.keys(value || {});
+   		const valid = first&&first.length>5 && rest.every(v => value[v] === value[first]);
+    	//const valid = control.value['password'] === control.value['pswconfirm'];
+    	return Observable.of(valid ? null : {confirm: true}).delay(2000);
+
+    }
+
+    public agreeValidator(control: FormControl): {[key: string]: boolean} {
+
+    	const value = control.value || '';
+    	const valid = value;
+    	return valid ? null : {emailError: true};
+
     }
 
     public loginSubmit(value: any):void {
---
+++ src/types/index.d.ts
@@ -0,0 +1,11 @@
+type Login = {
+	email: string,
+	password: number
+}
+
+type Registration = {
+	email: string,
+	password: number,
+	pswconfirm: number,
+	agree: boolean 
+}
